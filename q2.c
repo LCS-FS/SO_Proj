202be09cbd11dd8dec7522bf463042d477bc35a6
Lucas Sousa <lcsferreiradesousa@hotmail.com>
2022-05-05 14:07:22 +0100

    mmap of matrices and initial processes

--- q2.c
+++ q2.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[]){
     int n, m;
@@ -22,7 +23,16 @@ int main(int argc, char* argv[]){
     m = c-'0';
     c = fgetc(f);// \n
 
-    int m1[n][m], m2[n][m];//, ms[n][m];
+
+    //mmap
+    int *m1 = mmap(NULL, m*n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
+    int *m2 = mmap(NULL, m*n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
+    //int *ms = mmap(NULL, m*n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
+
+    if(m1 == MAP_FAILED || m2 == MAP_FAILED){
+        printf("map failed");
+        return 1;
+    }
     //read row by row
     for(int row = 0; row < n; row++){
         int cnt = 0;
@@ -31,7 +41,7 @@ int main(int argc, char* argv[]){
         while(c!='\n'){
             c = fgetc(f);
             if(c != ' ' && c != '\n'){
-                m1[row][cnt] = c - '0';
+                m1[m*row+cnt] = c - '0';
                 cnt++;
             }
         }
@@ -52,11 +62,32 @@ int main(int argc, char* argv[]){
         while(c!='\n'){
             c = fgetc(f);
             if(c != ' ' && c != '\n'){
-                m2[row][cnt] = c - '0';
+                m2[m*row+cnt] = c - '0';
                 cnt++;
             }
         }
     }
 
+    pid_t pids[m];
+    for(int i = 0; i < m; i++){
+        if((pids[i] = fork()) <0){
+            perror("fork");
+            return 1;
+        }
+        else if(pids[i] == 0){
+            printf("[child] pid %d from [parent] pid %d\n", getpid(), getppid());
+            return 0;
+        }
+    }
+
+    //wait for children
+    for(int i = 0; i < m; i++){
+        if (waitpid(pids[i], NULL, 0) == -1) {
+        perror("wait");
+        return EXIT_FAILURE;
+        }
+        printf("child nr %d ended\n", i);
+    }
+    printf("parent");
     return 0;
 }
